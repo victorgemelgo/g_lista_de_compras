a20d3ee5c540960b667fde22d5641a2dbba1ed4d
Victor <vigemelgo@gmail.com>
2024-09-24 20:25:48 -0300

    Criado card home

--- src/pages/HomeApp.vue
+++ src/pages/HomeApp.vue
@@ -9,8 +9,30 @@
         class="col-11"
       ></q-btn>
     </div>
-    <div class="row flex-center">
-      
+    <div class="row flex-center q-mt-md">
+      <q-card class="my-card col-11">
+        <q-card-section class="bg-primary text-white flex items-center justify-between">
+          <div class="text-h6">Nome da Lista</div>
+          <div class="text-subtitle2">00/00/00</div>
+        </q-card-section>
+
+        <q-card-actions class="flex justify-between">
+          <q-btn
+            flat
+            icon="edit"
+            color="primary"
+          >
+            Editar
+          </q-btn>
+          <q-btn
+            flat
+            icon="delete"
+            color="red"
+          >
+            Excluir
+          </q-btn>
+        </q-card-actions>
+      </q-card>
     </div>
   </div>
 </template>
